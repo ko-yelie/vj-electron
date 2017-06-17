7efc5ed3e2aa54d6b4d6d0d395d858543cf4c666
ko-yelie <ko.yelie@gmail.com>
2017-06-17 18:34:15 +0900

    モード切り替え

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,31 +1,18 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
+  <div>
+    <system-information></system-information>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    <p>{{ mode }}</p>
+    <p>
+      <button @click="control">コントロール</button>
+      <button @click="visual">ビジュアル</button>
+    </p>
+    <div id="control" v-if="mode === 'control'">
+      <p>コントロール画面</p>
+    </div>
+    <div id="visual" v-else-if="mode === 'visual'">
+      <p>ビジュアル画面</p>
+    </div>
   </div>
 </template>
 
@@ -35,9 +22,17 @@
   export default {
     name: 'landing-page',
     components: { SystemInformation },
+    computed: {
+      mode () {
+        return this.$store.state.Mode.mode
+      }
+    },
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+      control () {
+        this.$store.dispatch('control')
+      },
+      visual () {
+        this.$store.dispatch('visual')
       }
     }
   }
