bd87e359997b461f3177e7176df0fc043a89479f
ko-yelie <ko.yelie@gmail.com>
2017-06-18 19:24:55 +0900

    ビジュアル画面にYouTube表示

--- src/visualRenderer/App.vue
+++ src/visualRenderer/App.vue
@@ -1,25 +1,36 @@
 <template lang="pug">
-  p ビジュアル画面
+iframe.visual-frame(width="1024", height="768", :src="'https://www.youtube.com/embed/' + items[activeId].id + '?showinfo=0&rel=0&controls=0&fs=0&iv_load_policy=3&loop=1&autoplay=1'", frameborder="0", allowfullscreen="")
 </template>
 
 <script>
 export default {
-  name: 'visual-page'
+  name: 'visual-page',
+  data () {
+    return {
+      activeId: 0,
+      items: [
+        {
+          id: 'AovB1kid35o',
+          title: 'AOKI takamasa // rhythm variation 06'
+        },
+        {
+          id: 'ZQ1zjPqJBPQ',
+          title: 'Alva Noto + Opiate - Opto File 2 - 2001'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
-* {
-  box-sizing: border-box;
+body {
   margin: 0;
-  padding: 0;
 }
 
-body { font-family: 'Source Sans Pro', sans-serif; }
-
-p {
-  font-size: 2rem;
+.visual-frame {
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
