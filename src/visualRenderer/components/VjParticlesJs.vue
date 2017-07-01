e25790af7d4e89362972d2ff7c4aa41ef96c76e4
ko-yelie <ko.yelie@gmail.com>
2017-07-02 01:14:18 +0900

    mounted function

--- src/visualRenderer/components/VjParticlesJs.vue
+++ src/visualRenderer/components/VjParticlesJs.vue
@@ -4,31 +4,33 @@
 
 <script>
 import { ipcRenderer } from 'electron'
-import '../../../node_modules/particles.js/particles.js'
+import 'particles.js/particles.js'
+
+function mounted () {
+  let pJS
+
+  const actions = {
+    initParticlesJs: (configJson) => {
+      window.particlesJS(
+        'particles-js',
+        configJson
+      )
+      pJS = window.pJSDom[0].pJS
+
+      ipcRenderer.send('receive-particles-js', pJS)
+    },
+    updateParticlesJs: (pJSGui) => {
+      Object.assign(pJS, pJSGui)
+      pJS.fn.particlesRefresh()
+    }
+  }
+
+  ipcRenderer.on('dispatch-particles-js', (event, typeName, ...payload) => {
+    actions[typeName](...payload)
+  })
+}
 
 export default {
-  mounted () {
-    let pJS
-
-    const actions = {
-      initParticlesJs: (configJson) => {
-        window.particlesJS(
-          'particles-js',
-          configJson
-        )
-        pJS = window.pJSDom[0].pJS
-
-        ipcRenderer.send('receive-particles-js', pJS)
-      },
-      updateParticlesJs: (pJSGui) => {
-        Object.assign(pJS, pJSGui)
-        pJS.fn.particlesRefresh()
-      }
-    }
-
-    ipcRenderer.on('dispatch-particles-js', (event, typeName, ...payload) => {
-      actions[typeName](...payload)
-    })
-  }
+  mounted
 }
 </script>
